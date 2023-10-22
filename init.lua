42ea4dac3e7c24c653e5a9b8904d65289f31274b
olsemeniuk <ol.semeniuk.mail@gmail.com>
2023-10-22 13:53:49 +0300

    neovim config

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- Basic
+require('core.plugins')
+require('core.mappings')
+require('core.colors')
+require('core.configs')
+
+--Plugins
+require('plugins.neotree')
+require('plugins.treesitter')
+require('plugins.lsp')
+require('plugins.mason')
+require('plugins.lualine')
+require('plugins.autoclose')
+require('plugins.telescope')
+require('plugins.bufferline')
+require('plugins.highlight')
+require('plugins.alpha')
---
+++ lua/core/mappings.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+
+-- NeoTree
+vim.keymap.set('n', '<leader>e', ':Neotree left toggle<CR>', { silent = true })
+vim.keymap.set('n', '<leader>o', ':Neotree focus<CR>', { silent = true })
+
+
+-- Emmet
+vim.api.nvim_set_keymap('i', '<Tab>', 'emmet#expandAbbrIntelligent("\t")', 
+                        { noremap = true, expr = true, silent = true })
+
+
+-- Bufferline
+-- Go to the previous buffer
+vim.api.nvim_set_keymap('n', '<A-h>', '<Cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+
+-- Go to the next buffer
+vim.api.nvim_set_keymap('n', '<A-l>', '<Cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
+
+-- Close the current buffer
+vim.api.nvim_set_keymap('n', '<A-w>', '<Cmd>bdelete<CR>', { noremap = true, silent = true })
